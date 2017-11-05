9795f4f66861eb5e08f6e1d3ca34140a6163a59d
Jake Fryer <jfryer6@uwo.ca>
2017-11-05 01:23:11 -0400

    added basic Group class

---
+++ data/Group.h
@@ -0,0 +1,32 @@
+/*
+Group.h
+
+The Group class [...]
+
+@author - Jacob Fryer (jfryer6)
+@date - November 04, 2017
+*/
+
+#ifndef _group_h_included
+#define _group_h_included
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include "Light.h"
+
+class Group {
+    public:
+        Group();
+        
+        std::string getName();
+        Light getLight(int element);
+    private:
+        void addLight(Light l);
+        bool removeLight(Light l);
+        
+        std::string name;
+        std::vector<Light> lights;
+};
+
+#endif
