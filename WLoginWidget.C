2633adb8474f6684e5318e7c7d44f9d3ef65a248
Anthony Tran <atran94@uwo.ca>
2017-11-10 15:47:40 -0500

    Set the skeleton of WLoginWidget

--- WLoginWidget.C
+++ WLoginWidget.C
@@ -15,112 +15,62 @@
 #include <Wt/WText>
 #include <Wt/WBorderLayout>
 
+#include "User.h"
+#include "RegistrationWidget.h"
+
 using namespace Wt;
-
-/**
- * Phillips Hue
+using namespace std;
+/*
+ * Registration Widget
  */
-class WLoginWidgetApplication : public WApplication
-{
-public:
-  WLoginWidgetApplication(const WEnvironment& env);
-
-private:
-  WLineEdit *userNameEdit;
-  WLineEdit *passWordEdit;
-  WText *greeting_;
-
-  void greet();
-};
 
 
-/**
- * The env argument contains information about the new session, and
- * the initial request. It must be passed to the WApplication
- * constructor so it is typically also an argument for your custom
- * application constructor.
- * @param env information about the new session, and the initial request.
+/*
+ *Intializes and constructs the Registration Widget to be called by
+ *Hue main application.
  */
-WLoginWidgetApplication::WLoginWidgetApplication(const WEnvironment& env)
-  : WApplication(env)
+WLoginWidget::WLoginWidget(const std::string &name, WContainerWidget *parent)
+: WContainerWidget(parent), name_(name)
 {
-  setTitle("Phillips Hue Login");                               // application title
-  // show some text
+  setContentAlignment(AlignCenter);
+//  setTitle("Login Widget");
 
-  root()->addWidget(new WText("Enter your user name"));
-  userNameEdit = new WLineEdit(root());
-  userNameEdit->setFocus();
+  registerButton = new WPushButton("Login", this);
+  registerButton->clicked().connect(this, &RegistrationWidget::clearRegistration);
+  // userNameEdit = new WLineEdit(root());                     // allow text input
+  // userNameEdit->setFocus();                                 // give focus
 
-  root()->addWidget(new WText("Enter your password"));
-  passWordEdit = new WLineEdit(root());
-  passWordEdit->setFocus();
-
-
-  WPushButton *loginButton
-    = new WPushButton("Login", root());              // create a button
-  loginButton->setMargin(5, Top);                            // add 5 pixels margin
-  loginButton->setStyleClass("btn-primary");
-
-  WPushButton *signUpButton = new WPushButton("Sign Up", root());
-  signUpButton->setMargin(100, Bottom);
+  // passWordEdit = new WLineEdit(root());
+  // passWordEdit->setFocus();
 
 
-  loginButton->clicked().connect(this, &WLoginWidgetApplication::greet);
+  // WPushButton *loginButton
+  //   = new WPushButton("Login", root());              // create a button
+  // loginButton->setMargin(5, Top);                            // add 5 pixels margin
+  // loginButton->setStyleClass("btn-primary");
+
+  // WPushButton *signUpButton = new WPushButton("Sign Up", root());
+  // signUpButton->setMargin(100, Bottom);
 
 
-  /*
-   * - using an arbitrary function object (binding values with boost::bind())
-   */
-  userNameEdit->enterPressed().connect
-    (boost::bind(&WLoginWidgetApplication::greet, this));
-
-  /*
-   * - using a c++0x lambda:
-   */
-  // button->clicked().connect(std::bind([=]() {
-  //       greeting_->setText("Hello there, " + nameEdit_->text());
-  // }));
+  // loginButton->clicked().connect(this, &WLoginWidgetApplication::greet);
+  // userNameEdit->enterPressed().connect
+  //   (boost::bind(&WLoginWidgetApplication::greet, this));
 }
 
-
-/**
- * Update text to greet user.
- */
-void WLoginWidgetApplication::greet()
+/*This method is to create User after the user of programmer enters
+ *their information.
+*/
+/*User RegistrationWidget::createUser(std::string &userName, std::string &firstName, std::string &lastName, std::string &password)
 {
-  /*
-   * Update the text, using text input into the nameEdit_ field.
-   */
-  greeting_->setText("Hello there, " + userNameEdit->text());
+  User *newUser = new User(userName, password, firstName, lastName);
+
+  return *newUser;
 }
 
-
-/**
- * Read information from the environment to decide whether the user has permission to start a new application.
- * @param env environment
- * @return new application
- */
-WApplication *createApplication(const WEnvironment& env)
+/*This method is to clear the widget after user has succesfully logged in.
+*/
+void WLoginWidget::clearLogin()
 {
-  /*
-   * You could read information from the environment to decide whether
-   * the user has permission to start a new application
-   */
-  return new WLoginWidgetApplication(env);
-}
-
-int main(int argc, char **argv)
-{
-  /*
-   * Your main method may set up some shared resources, but should then
-   * start the server application (FastCGI or httpd) that starts listening
-   * for requests, and handles all of the application life cycles.
-   *
-   * The last argument to WRun specifies the function that will instantiate
-   * new application objects. That function is executed when a new user surfs
-   * to the Wt application, and after the library has negotiated browser
-   * support. The function should return a newly instantiated application
-   * object.
-   */
-  return WRun(argc, argv, &createApplication);
+  std::cout << "Login Widget Cleared";
 }
