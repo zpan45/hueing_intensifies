231ef398dcc8cfd738b80c618cd2bb45a0aa993c
Jake Fryer <jfryer6@uwo.ca>
2017-11-30 20:47:17 -0500

    added error dialogues to the register page

--- frontend/RegistrationWidget.cpp
+++ frontend/RegistrationWidget.cpp
@@ -42,10 +42,15 @@ RegistrationWidget::RegistrationWidget(const std::string &name, User* current, W
     passwordEdit = new WLineEdit(this);
     passwordEdit->setEchoMode(Wt::WLineEdit::EchoMode::Password);
     this->addWidget(new Wt::WBreak());
+    this->addWidget(new Wt::WText("Confirm Password: "));
+    passwordConf = new WLineEdit(this);
+    passwordConf->setEchoMode(Wt::WLineEdit::EchoMode::Password);
+    this->addWidget(new Wt::WBreak());
 
     // add the first and last name entry text boxes
     this->addWidget(new Wt::WText("First Name: "));
     firstNameEdit = new WLineEdit(this);
+    this->addWidget(new Wt::WBreak());
     this->addWidget(new Wt::WText("\tLast Name: "));
     lastNameEdit = new WLineEdit(this);
     this->addWidget(new Wt::WBreak());
@@ -53,6 +58,13 @@ RegistrationWidget::RegistrationWidget(const std::string &name, User* current, W
     // add the register button
     registerButton = new WPushButton("Register", this);
     registerButton->clicked().connect(this, &RegistrationWidget::createUser);
+    
+    this->addWidget(new Wt::WBreak());
+    this->addWidget(new Wt::WBreak());
+    this->addWidget(new Wt::WBreak());
+    
+    // set up a spot for an error message to be displayed
+    //Wt::WText *errorText_ = new Wt::WText(this);
 }
 
 /**
@@ -66,32 +78,69 @@ RegistrationWidget::RegistrationWidget(const std::string &name, User* current, W
  * @todo - Return the User object somewhere for use in the rest of the app
  */
 void RegistrationWidget::createUser() {
+    
 
-    if (usernameEdit->text().toUTF8() == "" || passwordEdit->text().toUTF8() == "" || firstNameEdit->text().toUTF8() == "" || lastNameEdit->text().toUTF8() == "" )
+    if (usernameEdit->text().toUTF8() == "" || passwordEdit->text().toUTF8() == "" || passwordConf->text().toUTF8() == "" || firstNameEdit->text().toUTF8() == "" || lastNameEdit->text().toUTF8() == "" )
     {
-        this->addWidget(new Wt::WText("ERROR: Can't register with an empty field! Make sure all fields are filled in and try again."));
-        this->clearRegistration();
+        
+        showError(true, "Can't register with an empty field! Make sure all fields are filled in and try again.");
+        //errorText_->setText(Wt::WString("ERROR: Can't register with an empty field! Make sure all fields are filled in and try again."));
+        //this->clearRegistration();
     }
     else
     {
-        User newUser(usernameEdit->text().toUTF8(), passwordEdit->text().toUTF8(), firstNameEdit->text().toUTF8(), lastNameEdit->text().toUTF8());
-
-        this->addWidget(new Wt::WBreak());
-        this->addWidget(new Wt::WBreak());
-        this->addWidget(new Wt::WBreak());
-
-        bool result = ::activeDB.DBFileManager::addNewUser(newUser);
-
-        if(result)
-        {
-            this->addWidget(new Wt::WText("ERROR: User already exists. Please try with a different username."));
+        if( passwordEdit->text().toUTF8() != passwordConf->text().toUTF8() ) {
+            showError(true, "The passwords entered do not match! Please try again.");
         }
-        else
-        {
-            this->addWidget(new Wt::WText( newUser.constructGreetingString() ));
+        else{
+            User newUser(usernameEdit->text().toUTF8(), passwordEdit->text().toUTF8(), firstNameEdit->text().toUTF8(), lastNameEdit->text().toUTF8());
+
+            bool result = ::activeDB.DBFileManager::addNewUser(newUser);
+
+            if(result)
+            {
+                showError(true, "User already exists. Please try with a different username.");
+            }
+            else
+            {
+                showError(false, newUser.constructGreetingString() + " Redirecting to Login screen...");
+            }
         }
     }
 
+}
+
+/** Method that displays a dialogue box for errors (or successful registration).
+* @brief Display an error message to the user.
+* @param isError - a boolean to determine whether the message is an error or successful registration
+* @param errorMessage - a string with which to construct the error message's body.
+*/
+void RegistrationWidget::showError(bool isError, string errorMessage) {
+    Wt::WDialog errorText_;
+    
+    if(isError == true) {
+        errorText_.setWindowTitle("ERROR");
+    }
+    else {
+        errorText_.setWindowTitle("Hello!");
+    }
+    
+    
+    Wt::WText errMessage_(errorMessage, errorText_.contents());
+    new Wt::WBreak(errorText_.contents());
+    new Wt::WBreak(errorText_.contents());
+    
+    Wt::WPushButton confirm("Okay", errorText_.contents());
+    
+    confirm.clicked().connect(&errorText_, &Wt::WDialog::accept);
+    
+    if(errorText_.exec() == Wt::WDialog::Accepted) {
+        
+        if(isError == false) {
+            Wt::WApplication::instance()->setInternalPath("/login", true);
+        }
+        
+    }
 }
 
 /**
